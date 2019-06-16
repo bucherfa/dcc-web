a282d0e77262fef0604acb7b0f5dd4f1707cdd5f
Fabian Bucher <git@fabianbucher.de>
2019-06-16 17:44:00 +0200

    deploy

---
+++ precache-manifest.c4817de8bf55ab2cc45ede382b627f35.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5c3e83a4fe1eb3e8bbe",
+    "url": "/dcc-web/css/app.e4b83c46.css"
+  },
+  {
+    "revision": "d5c3e83a4fe1eb3e8bbe",
+    "url": "/dcc-web/js/app.83368184.js"
+  },
+  {
+    "revision": "94c50daa9f8e922d3348",
+    "url": "/dcc-web/js/chunk-vendors.1a2532d5.js"
+  },
+  {
+    "revision": "b0b93ef82f3cee098ab603efdd16e752",
+    "url": "/dcc-web/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/dcc-web/robots.txt"
+  }
+];
